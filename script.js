aa4909820fa8ba2a44212d50506dccc99274c029
singhalbhavik <64714643+singhalbhavik@users.noreply.github.com>
2022-01-09 22:10:02 -0800

    Create script.js

---
+++ script.js
@@ -0,0 +1,40 @@
+var nba_articles = [
+  {
+    image: "https://cdn.nba.com/manage/2021/03/2022-all-star-logo-primary-v2.jpeg",
+    title: "2021-22 NBA All-Star Predictions",
+    description: "In this article, Sporting News discusses NBA all-star locks, potential reserves, and snubs.",
+    link: "https://www.sportingnews.com/us/nba/news/nba-all-star-game-2022-locks-reserves-snubs/17ypbgp9sawzp1ortg8ois99io"
+  },
+  {
+    image: "https://www.nbcsports.com/sites/rsnunited/files/styles/metatags_opengraph/public/gallery/hero/Durant_Curry_James.jpg",
+    title: "Top 20 NBA Players This Year",
+    description: "Here, NBC Sports ranks the Top 20 players right now disregarding injuries.",
+    link: "https://www.nbcsports.com/washington/wizards/2022-ranking-top-20-nba-players-right-now"
+  },
+  {
+    image: "https://miro.medium.com/max/1400/1*H9iXbBguPJNe8eJVUZYm9g.jpeg",
+    title: "NBA Stats for 2021-22 Season",
+    description: "Take a look at who's leading the league in scoring and rebounding with this link. Basketball Reference provides you all the stats for this year's NBA season.",
+    link: "https://www.basketball-reference.com/leagues/NBA_2022_leaders.html"
+  },
+  {
+    image: "https://cdn.vox-cdn.com/thumbor/KiVchyTZyvsBgnl1op2ITFNT2tI=/0x0:1820x1213/1200x800/filters:focal(757x236:1047x526)/cdn.vox-cdn.com/uploads/chorus_image/image/70335896/MVP.0.png",
+    title: "NBA MVP Predictions",
+    description: "The NBA website discusses the MVP race going on and the projected MVP winner for the 2021-22 season as of now.",
+    link: "https://www.nba.com/news/kia-mvp-ladder-jan-7-2022-edition"
+  },
+  {
+    image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTSU6SJkELg3KpgTcMgNrXmQ3lEwjknf_RwGQ&usqp=CAU",
+    title: "NBA Championship Predictions",
+    description: "SportsNaut predicts the NBA standings by the end of the 2021-22 season. They also give a playoff picture and predict how the playoffs will play out and end by announcing their prediction for the 2021-22 championship.",
+    link: "https://sportsnaut.com/nba-playoff-predictions-east-west/"
+  },
+  {
+    image: "https://cloudfront-us-east-2.images.arcpublishing.com/reuters/7347ZJZE5NJQ3G6X4QNUR6GHCQ.jpg",
+    title: "NBA Covid Protocols",
+    description: "CBS Sports gives the games that were suspended due to Covid-19 Protocols and also lists the players who are currently out because of the protocols.",
+    link: "https://www.cbssports.com/nba/news/nba-covid-tracker-team-by-team-list-as-grizzlies-coach-taylor-jenkins-jazzs-rudy-gobert-enter-protocols/"
+  }
+  
+]
+
